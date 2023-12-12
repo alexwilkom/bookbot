2264c873eb508c853a4ce1a96762174d4f2045d5
Alex Wilkom <alex.wilkom@icloud.com>
2023-12-12 23:43:24 +0100

    Add report function

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
     number_words = get_num_words(text)
-    print(text[:1000])
-    print(number_words)
+    chars_dict = get_num_letters(text)
+    print_report(book_path, number_words, chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,4 +23,15 @@ def get_num_letters(text):
             chars[char] += 1
     return chars
 
+def print_report(path, words, chars):
+    print(f"--- Begin report of {path} ---")
+    print(f"{words} found in the document")
+    print("\n")
+    for key, value in chars.items():
+        if not key.isalpha():
+            continue
+        print(f"The '{key}' character was found {value} times")
+    print("\n")
+    print("--- End report ---")
+    
 main()
